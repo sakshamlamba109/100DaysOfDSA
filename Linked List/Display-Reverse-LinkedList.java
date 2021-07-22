35a05b4cf7e76cf3b8741fc14c83ce39a1bf3767
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-07-22 15:16:55 +0530

    K-Reverse-LinkedList (Without actually reversing using recursion

---
+++ Linked List/Display-Reverse-LinkedList.java
@@ -0,0 +1,14 @@
+private void displayReverseHelper(Node node)
+{   if(node==null)
+    {
+        return;
+    }
+       displayReverseHelper(node.next);
+       System.out.print(node.data + " ");
+}
+    
+     public void displayReverse()
+  {
+         displayReverseHelper(head);
+         System.out.println();
+  }
