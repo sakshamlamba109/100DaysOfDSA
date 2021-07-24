0c2627f7c76f900fb691794d6509f3ba013a05e8
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-07-24 09:59:33 +0530

    Fold a Linked List

---
+++ Linked List/Fold a Linked List.java
@@ -0,0 +1,43 @@
+private static void foldHelp(Node right, int t)
+{   
+     if(right==null)
+        
+     { 
+
+       return;
+     
+     }
+
+     foldHelp(right.next, t+1);
+ 
+     if(t>size/2)
+
+    {
+     
+      Node temp=left;
+      left=left.next;
+      temp.next=right;
+      right.next=temp;
+
+    }
+     
+    else if(t==size/2)
+
+    {
+   
+       tail=right;
+       right.next==null;
+   
+     }
+
+}
+
+Node left=null;
+
+public static void fold()
+{
+  left=head;
+  foldHelp(head,0);
+}
+
+       
