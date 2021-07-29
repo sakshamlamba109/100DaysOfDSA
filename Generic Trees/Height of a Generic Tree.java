63d52e46cb7cb4f9e7f831d93913949a368d3a52
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-07-29 11:12:42 +0530

    Height of a Generic tree

---
+++ Generic Trees/Height of a Generic Tree.java
@@ -0,0 +1,19 @@
+public static int height(Node node)
+{
+
+  int h=-1;
+
+ for(Node child:node.children)
+{
+
+  int tcom=height(child);
+  
+  h=Math.max(h,tcom);
+
+}
+
+ h=h+1;
+
+ return h;
+
+}
