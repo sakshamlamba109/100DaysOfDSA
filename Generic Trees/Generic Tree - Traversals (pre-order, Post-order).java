f3ae6badd84b2d7c947376761ef2a18c1493ea70
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-07-29 11:41:18 +0530

    Generic Tree - Traversals (pre-order, Post-order)

---
+++ Generic Trees/Generic Tree - Traversals (pre-order, Post-order).java
@@ -0,0 +1,21 @@
+public static void treversal(Node node)
+{
+   System.out.println("Node Pre"+node.data);
+
+   for(Node child:node.children)
+
+   {
+      
+      System.out.println("Edge Pre"+node.data+ "--"+child.data);
+
+      traversal(child);
+
+      System.out.println("Edge Post"+node.data+ "--"+child.data);
+      
+    }
+
+    System.out.println("Node Post"+node.data);
+
+    return;
+
+}
--- Generic Trees/Height of a Generic Tree.java
+++ Generic Trees/Height of a Generic Tree.java
@@ -17,3 +17,5 @@ public static int height(Node node)
  return h;
 
 }
+
+
