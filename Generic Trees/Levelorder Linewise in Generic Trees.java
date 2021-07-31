1577c63f8631f7ebc15b34b9c26e84534c360384
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-07-31 23:42:59 +0530

    Level-Order Linewise in Generic Tree

---
+++ Generic Trees/Levelorder Linewise in Generic Trees.java
@@ -0,0 +1,24 @@
+  public static void levelOrderLinewise(Node node){
+    Queue<Node> temp=new ArrayDeque<>();
+    Queue<Node> t=new ArrayDeque<>();
+    temp.add(node);
+    
+    while(temp.size()>0)
+    {
+        node=temp.remove();
+        System.out.print(node.data + " ");
+       
+    for(Node child:node.children)
+    {
+        
+        t.add(child);
+    }
+    if(temp.size()==0)
+    {
+        temp=t;
+        t=new ArrayDeque<>();
+        System.out.println();
+    }
+  }
+  
+  }
