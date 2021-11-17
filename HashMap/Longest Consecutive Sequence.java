98cfa8dade7a37acdf62a7c9731cca35a387a566
Saksham <500075568@stu.upes.ac.in>
2021-11-17 23:05:03 +0530

    DAY 53

---
+++ HashMap/Longest Consecutive Sequence.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int [] arr=new int[n];
+    for(int i=0;i<arr.length;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+    HashMap<Integer,Boolean> map=new HashMap<>();
+ }
+ for(int val : arr)
+ {
+     if(map.containsKey(val-1))
+     {
+         map.put(val,false);
+     }
+ }
+ int msp=0;
+ int ml=0;
+ for(int val:arr)
+ {
+     if(map.get(val)==true)
+     {
+         int tl=1;
+         int tsp=val;
+         
+         while(map.containsKey(tsp+tl))
+         {
+             tl++;
+         }
+         if(tl>ml)
+         {
+             msp=tsp;
+             ml=tl;
+         }
+     }
+ }
+ for(int i=0 ; i<ml ;i++)
+ {
+     System.out.println(msp+1);
+ }
+
+}
